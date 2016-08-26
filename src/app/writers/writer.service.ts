27d783571d9fcac653e59593a772cd0a4d886fa5
Norman <ua.norman@gmail.com>
2016-08-26 16:55:10 +0200

    Doc was updated

--- src/app/writers/writer.service.ts
+++ src/app/writers/writer.service.ts
@@ -1,4 +1,3 @@
-import {Book} from "../books/book";
 /**
  * Created by Norman on 05/08/2016.
  */
@@ -6,6 +5,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import {Writer} from "./writer";
+import {Book} from "../books/book";
 
 /**
  * This service will manage the operations related to Writers against the API Rest Backend
@@ -18,12 +18,17 @@ export class WriterService {
   private apiUrl = "http://localhost:3000/writers";
   /**
    * Fetch all the writers and return them into array
-   * @returns {Promise<Writer[]>}
+   * @returns {JSON} -> Array of writers
    */
   getWriters() : Observable<Writer[]>{
     return this.http.get(this.apiUrl).map(this.extractData);
   }
 
+  /**
+   * Utility function for extracting date from Observable Response. Extracts and return the data.
+   * @param  {Response} res-> Observable Response
+   * @return {Json} -> Json data
+   */
   private extractData(res: Response) {
     let body = res.json();
     return body.data || { };
@@ -32,7 +37,7 @@ export class WriterService {
   /**
    * Look up and return the writer that match with the id passed.
    * @param id -> Id of the writer which is going to be retrieved
-   * @returns {Promise<TResult>}-> Promise containing the writer found
+   * @returns {JSON}-> Writer JSON Object
    */
   getWriter(id: string): Observable<Writer> {
     const url = this.apiUrl+"/"+id;
@@ -42,7 +47,7 @@ export class WriterService {
   /**
    * Fetch all the books that belong to the Writer
    * @param id-> Id of the writer
-   * @returns {Promise<TResult>} -> Promise containing all the books of a Writer
+   * @returns {JSON} -> Array of books
    */
   getBooks(id: string): Observable <Book[]>{
     const url = this.apiUrl+"/"+id+"/books";
